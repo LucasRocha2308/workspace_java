3388d81f58c9e899247501b0a36142c17d9ba373
LucasRocha2308 <lucas_rocha_25@hotmail.com>
2022-02-12 22:08:40 -0300

    ëxercicio de elementos static

---
+++ ProjetoStatic/src/TesteUtilitarios.java
@@ -0,0 +1,12 @@
+import br.com.lucascorp.projetostatic.util.Utilitarios;
+
+public class TesteUtilitarios {
+	public static void main(String[] args) {
+//		Utilitarios u = new Utilitarios();
+		
+		System.out.println(Utilitarios.soma(2, 3));
+		System.out.println(Utilitarios.subtrair(50, 1));
+		System.out.println(Utilitarios.pot(2, 5));
+		System.out.println(Utilitarios.PI);
+	}
+}
